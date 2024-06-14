71b6ea0a01cf331a6ca88444448d737c647b31c3
Anna Musial <aiwmusial@gmail.com>
2024-06-14 22:22:11 +0200

    project initialisation

---
+++ vue-form/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1 class="container text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
